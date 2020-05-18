70f51d206c05bedc2f88bbd004f051a083a1f124
mfauzanalg <mfauzanalg@gmail.com>
2020-05-18 19:47:01 +0700

    CheckPoint layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <!-- Date -->
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.css') }}">  
--- resources/views/output.blade.php
+++ resources/views/output.blade.php
@@ -3,6 +3,39 @@
 @section('title', 'output')
 
 @section('content')
-    
+    <!-- Jumbotron -->
+    <div class="jumbotron text-center p-3 mb-5 bg-white rounded">
+      <div class="container">
+        <h1>Daftar Kerja Magang</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      </div>
+    </div>
+
+    <div class="data rounded">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">Tanggal</th>
+                <th scope="col">Jam-Masuk</th>
+                <th scope="col">Jam-Keluar</th>
+                <th scope="col">Kendala</th>
+                <th scope="col">Pekerjaan</th>
+                <th scope="col">Waktu Kerja</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th scope="row">12 Maret 2019</th>
+                    <td>13:30</td>
+                    <td>12:20</td>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, nobis architecto saepe odit vitae facere ab nihil veritatis distinctio, repellat fugit, delectus amet adipisci et quae molestiae quod pariatur quisquam.</td>
+                    <td>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo enim neque excepturi molestias nemo ex dignissimos ratione fugiat odit architecto? Nulla omnis aliquid eveniet! Ullam libero ut ab repellat numquam!</td>
+                    <td>Apa</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 
 @endsection
