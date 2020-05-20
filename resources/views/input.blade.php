59a1a2490e65938d8fb3ec955c94571f05148adf
mfauzanalg <mfauzanalg@gmail.com>
2020-05-20 17:42:23 +0700

    Update input.blade.php

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -19,20 +19,20 @@
                     <div class="dates" style="color:#white;">
                         <!-- Data Input Field -->
                         <h3>Tanggal</h3>
-                            <input type="text" style="width:200px;background-color:#whute;" class="form-control tanggal" id="usr1" name="event_date" placeholder="YYYY-MM-DD" autocomplete="off" >
+                            <input autocomplete="off" type="text" style="width:200px;background-color:#whute;" class="form-control tanggal" id="usr1" name="event_date" placeholder="YYYY-MM-DD" autocomplete="off" >
                     </div>
                 </div>
 
                 <!-- Time Input Field -->
                 <div class="col-md-4">
                     <h3>Jam Masuk</h3>
-                    <input id="timepkr" style="width:200px;float:left;" class="form-control jam_masuk" placeholder="HH:MM" />
+                    <input autocomplete="off" id="timepkr" style="width:200px;float:left;" class="form-control jam_masuk" placeholder="HH:MM" />
                     <button class="btn btn-danger" onclick="showpickers('timepkr',24)" style="width:40px;float:left;"><i class="fa fa-clock-o"></i>
                 </div>
                 <div class="timepicker"></div>  <!--Important to add and keep it seprate to the end-->
                 <div class="col-md-4">
                     <h3>Jam Pulang</h3>
-                    <input id="timepkr2" style="width:200px;float:left;" class="form-control jam_keluar" placeholder="HH:MM" />
+                    <input autocomplete="off" id="timepkr2" style="width:200px;float:left;" class="form-control jam_keluar" placeholder="HH:MM" />
                     <button class="btn btn-danger" onclick="showpickers('timepkr2',24)" style="width:40px;float:left;"><i class="fa fa-clock-o"></i>
                 </div>
                 <div class="timepicker"></div>  <!--Important to add and keep it seprate to the end-->
@@ -44,7 +44,7 @@
         <!-- Work Description -->
         <div class="form-group time">
             <h3>Deskripsi Pekerjaan</h3>
-            <input type="text" class="input_pekerjaan form-control" placeholder="Pekerjaan"></textarea>
+            <input autocomplete="off" type="text" class="input_pekerjaan form-control" placeholder="Pekerjaan"></textarea>
         </div>
 
         <br>
@@ -52,7 +52,7 @@
         <!-- obstacles encountered -->
         <div class="form-group time">
             <h3>Kendala yang dihadapi</h3>
-            <input type="text" class="input_kendala form-control" placeholder="Kendala"></input>
+            <input autocomplete="off" type="text" class="input_kendala form-control" placeholder="Kendala"></input>
         </div>
    
         <br>
